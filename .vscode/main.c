e1dd14ebb7a4917713f0f63083d5800c0a143c87
henry.zhao <henry.zhao@veoneer.com>
2019-04-14 12:28:57 +0800

    Add VsCode project

---
+++ .vscode/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+unsigned int i[]= {1,3,2,4,4,5};
+int main(){
+    int iFirstVariable = 0x22;
+    printf("Hello W/*sdasd*/orld\n");
+    system("pause");//系统sttop;
+    return 0;
+}
