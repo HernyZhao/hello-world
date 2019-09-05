c0d351a2a237c28b9d729fbb7b254b49d2798235
HenryZhao <jyzhaoshuai@163.com>
2019-09-05 14:22:21 +0800

    commit 2

--- .vscode/main.c
+++ .vscode/main.c
@@ -4,7 +4,11 @@ unsigned int i[]= {1,3,2,4,4,5};
 int main(){
     int iFirstVariable = 0x22;
     printf("Hello W/*sdasd*/orld\n");
+<<<<<<< HEAD
 	printf("TestHeadCommit1");
+	printf("TestHeadCommit2");
+=======
+>>>>>>> parent of b1a0bb0... commit1 in branch
     system("pause");//系统sttop;
     return 0;
 }
